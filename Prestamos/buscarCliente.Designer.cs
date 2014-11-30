9bf3c303ffdcde53aab5951efabdefbe5fc1792d
ParadoX <edward_0-0-7@hotmail.com>
2014-11-30 00:01:23 -0400

    Clase buscarCliente creada

---
+++ Prestamos/buscarCliente.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Prestamos
+{
+    partial class buscarCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bcDataGridView = new System.Windows.Forms.DataGridView();
+            this.bcBusqueda = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bcDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bcDataGridView
+            // 
+            this.bcDataGridView.AllowUserToAddRows = false;
+            this.bcDataGridView.AllowUserToDeleteRows = false;
+            this.bcDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.bcDataGridView.Location = new System.Drawing.Point(32, 65);
+            this.bcDataGridView.MultiSelect = false;
+            this.bcDataGridView.Name = "bcDataGridView";
+            this.bcDataGridView.ReadOnly = true;
+            this.bcDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.bcDataGridView.Size = new System.Drawing.Size(394, 150);
+            this.bcDataGridView.TabIndex = 7;
+            this.bcDataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.bcDataGridView_CellDoubleClick);
+            // 
+            // bcBusqueda
+            // 
+            this.bcBusqueda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bcBusqueda.Location = new System.Drawing.Point(144, 28);
+            this.bcBusqueda.Name = "bcBusqueda";
+            this.bcBusqueda.Size = new System.Drawing.Size(178, 22);
+            this.bcBusqueda.TabIndex = 5;
+            this.bcBusqueda.TextChanged += new System.EventHandler(this.bcBusqueda_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Buscar cliente";
+            // 
+            // buscarCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 242);
+            this.Controls.Add(this.bcDataGridView);
+            this.Controls.Add(this.bcBusqueda);
+            this.Controls.Add(this.label1);
+            this.Name = "buscarCliente";
+            this.Text = "buscarCliente";
+            ((System.ComponentModel.ISupportInitialize)(this.bcDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView bcDataGridView;
+        private System.Windows.Forms.TextBox bcBusqueda;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Prestamos/buscarCliente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prestamos
+{
+    public partial class buscarCliente : Form
+    {
+        public buscarCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void bcBusqueda_TextChanged(object sender, EventArgs e)
+        {
+            TextBox sen = (TextBox)sender;
+
+            if (sen.Text.ToString().Trim() != "" && Convert.ToInt16(bcDataGridView.Rows[0].Cells[0].Value.ToString()) >= 1)
+            {
+                for (int i = 0; i < bcDataGridView.Rows.Count; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (bcDataGridView.Rows[i].Cells[j].Value.ToString().Contains(sen.Text.ToString()))
+                        {
+                            bcDataGridView.Rows[i].Cells[j].Selected = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void bcDataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show(bcDataGridView.Rows[e.RowIndex].Cells[1].Value.ToString());
+        }
+    }
+}
